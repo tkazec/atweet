a97b784f188c1b2f6a93e87167d6bf6bdd8228c0
Teddy Cross <tkazec@gmail.com>
2012-08-19 01:00:34 -0400

    LESS!

--- render.js
+++ render.js
@@ -4,13 +4,21 @@ var fs = require("fs");
 
 var ff = require("ff");
 var jade = require("jade");
+var less = require("less");
 
 var user = require("./gen/tkazec");
 
 var f = ff(function () {
-	jade.renderFile("static/stats.jade", user, f());
+	less.render("@import 'stats';", {
+		paths: [__dirname + "/static"],
+		compress: true
+	}, f());
+}, function (css) {
+	user.css = css;
+	
+	jade.renderFile(__dirname + "/static/stats.jade", user, f());
 }, function (html) {
-	fs.writeFile("gen/stats.html", html, f());
+	fs.writeFile(__dirname + "/gen/stats.html", html, f());
 }).cb(function (err) {
 	console.log("All done!", err);
 });
