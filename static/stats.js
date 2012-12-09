51d662373c194ac3cf72e8b2d4f8bdd70d9d31b0
Teddy Cross <tkazec@gmail.com>
2012-12-09 09:19:23 -0800

    add scaling and multiple chart support

--- static/stats.js
+++ static/stats.js
@@ -1,20 +1,39 @@
-var svg = d3.select("#stats-timeline");
+var charts = {
+	"#stats-timeline": function (svg, width, height) {
+		var scaleX = d3.scale.linear()
+			.domain([0, data.timeline.length])
+			.range([0, width]);
+		
+		var scaleY = d3.scale.linear()
+			.domain([0, d3.max(data.timeline, function (d) {
+				return d ? d.tweets + d.retweets + d.replies : 0;
+			})])
+			.range([0, height]);
+		
+		var padding = 1;
+		
+		svg.selectAll("rect")
+			.data(data.timeline)
+			.enter()
+			.append("rect")
+			.attr("x", function (d, i) {
+				return scaleX(i);
+			})
+			.attr("y", function (d) {
+				return height - scaleY(d && d.tweets + d.retweets + d.replies);
+			})
+			.attr("width", width / data.timeline.length - padding)
+			.attr("height", function (d) {
+				return scaleY(d && d.tweets + d.retweets + d.replies);
+			});
+	},
+	"#stats-types": function (svg, width, height) {
+		
+	}
+};
 
-var width = svg.attr('width');
-var height = svg.attr('height');
-var padding = 1;
-
-svg.selectAll("rect")
-	.data(data.timeline)
-	.enter()
-	.append("rect")
-	.attr("x", function (d, i) {
-		return i * (width / data.timeline.length);
-	})
-	.attr("y", function (d) {
-		return height - (d * 4);
-	})
-	.attr("width", width / data.timeline.length - padding)
-	.attr("height", function (d) {
-		return d * 4;
-	});
+Object.keys(charts).forEach(function (k) {
+	var svg = d3.select(k);
+	
+	charts[k](svg, svg.attr("width"), svg.attr("height"));
+});
